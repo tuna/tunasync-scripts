41e933da6a49755ce93158ed3d8510fa52bcdfe6
bigeagle <justin.w.xd@gmail.com>
2016-07-27 23:29:25 +0800

    aosp bitmap index

--- aosp.sh
+++ aosp.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+set -e
 
 REPO=${REPO:-"/usr/local/bin/repo"}
+USE_BITMAP_INDEX=${USE_BITMAPT_INDEX:-"0"}
 
 function repo_init() {
 	mkdir -p $TUNASYNC_WORKING_DIR
@@ -13,9 +15,25 @@ function repo_sync() {
 	$REPO sync -f
 }
 
-if [ ! -d "$TUNASYNC_WORKING_DIR/git-repo.git" ]; then
+function git_repack() {
+	echo "Start writing bitmap index"
+	while read repo; do 
+		cd $repo
+		size=$(du -sm .|cut -f1)
+		if [[ "$size" -gt "100" ]]; then
+			echo $repo, ${size}M
+			git repack -a -b -d
+		fi
+	done < <$(find $TUNASYNC_WORKING_DIR -type d -not -path "*/.repo/*" -name "*.git")
+}
+
+if [[ ! -d "$TUNASYNC_WORKING_DIR/git-repo.git" ]]; then
 	echo "Initializing AOSP mirror"
 	repo_init
 fi
 
 repo_sync
+
+if [[ "$USE_BITMAP_INDEX" == "1" ]]; then
+	git_repack
+do
