827203c2047a44fd598b4f7adf248aba7d432ebe
bigeagle <justin.w.xd@gmail.com>
2016-10-07 15:38:46 +0800

    steamos

---
+++ steamos.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+function sync_steamos() {
+	repo_url="$1"
+	repo_dir="$2"
+
+	[ ! -d "$repo_dir" ] && mkdir -p "$repo_dir"
+	cd $repo_dir
+	lftp "${repo_url}/" -e "mirror --verbose  -P 5 --delete --only-newer; bye"
+}
+
+BASE_URL=${TUNASYNC_UPSTREAM_URL:-"http://repo.steampowered.com"}
+sync_steamos "${BASE_URL}" "${TUNASYNC_WORKING_DIR}"
