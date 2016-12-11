a8426387fc05b4afc0363e85d371f37b48bbf753
bigeagle <justin.w.xd@gmail.com>
2016-12-11 17:09:25 +0800

    update rubygems and the Dockerfile for rubygems

--- rubygems.sh
+++ rubygems.sh
@@ -6,6 +6,7 @@ CONF="$HOME/.gem/.mirrorrc"
 mkdir -p "$HOME/.gem"
 
 INIT=${INIT:-"0"}
+BUSYBOX=${BUSYBOX:-"0"}
 
 if [ ! -d "$TUNASYNC_WORKING_DIR" ]; then
 	mkdir -p $TUNASYNC_WORKING_DIR
@@ -14,36 +15,29 @@ fi
 
 echo "Syncing to $TUNASYNC_WORKING_DIR"
 
-if [[ $INIT == "0" ]]; then
-
-	cat > $CONF << EOF
+cat > $CONF << EOF
 ---
 - from: https://rubygems.org
-  to: ${TUNASYNC_WORKING_DIR}
-  parallelism: 10
-  retries: 2
-  delete: true
-  skiperror: true
+to: ${TUNASYNC_WORKING_DIR}
+parallelism: 10
+retries: 2
+delete: true
+skiperror: true
 EOF
 
-	/usr/bin/timeout -s INT 7200 $GEM mirror  
-	if [[ $? == 124 ]]; then
-		echo 'Sync timeout (/_\\)'
-		exit 1
+if [[ $INT == "0" ]]; then
+	if [[ $BUSYBOX == "0" ]]; then
+		timeout -t 7200 -s INT $GEM mirror -V
+	else
+		timeout -s INT 7200 $GEM mirror -V
 	fi
-
 else
-
-	cat > $CONF << EOF
----
-- from: https://rubygems.org
-  to: ${TUNASYNC_WORKING_DIR}
-  parallelism: 10
-  retries: 2
-  delete: true
-  skiperror: true
-EOF
-
-	$GEM mirror
-
+	$GEM mirror -V
 fi
+
+ret=$?
+if [[ $ret == 124 ]]; then
+	echo 'Sync timeout (/_\\)'
+fi
+
+exit $ret
