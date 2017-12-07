36bb82a57dcea446362ef7fddd1767a679ec0005
王邈 <shankerwangmiao@gmail.com>
2017-12-08 04:52:11 +0800

    add script for winehq

---
+++ winehq.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+function sync_winehq() {
+	repo_url="$1"
+	repo_dir="$2"
+
+	[ ! -d "$repo_dir" ] && mkdir -p "$repo_dir"
+	cd $repo_dir
+	lftp "${repo_url}/" -e "mirror --verbose --exclude wine-builds.old/ -P 5 --delete --only-newer; bye"
+}
+
+BASE_URL=${TUNASYNC_UPSTREAM_URL:-"ftp://ftp.winehq.org/pub/"}
+sync_winehq "${BASE_URL}" "${TUNASYNC_WORKING_DIR}"
