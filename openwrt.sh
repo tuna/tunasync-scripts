93692652dda6532efd27b0bca4686d4fcc5bd037
Hui Yiqun <huiyiqun@gmail.com>
2018-08-10 00:46:30 +0800

    Revert "begin to sync new openwrt and stop the old one"
    This reverts commit 227b3207807e3687b645315dd69534c9a2449f33.

--- openwrt.sh
+++ openwrt.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-CURRENT_STABLE_RELEASES="17.01.5 18.06.0"
-
 function sync_openwrt() {
 	repo_url="$1"
 	repo_dir="$2"
@@ -12,5 +10,4 @@ function sync_openwrt() {
 	lftp "${repo_url}/" -e "mirror --verbose -P 5 --only-newer --exclude-glob *.ipk; bye"
 }
 
-#sync_openwrt "http://downloads.openwrt.org/chaos_calmer/15.05.1" "${TUNASYNC_WORKING_DIR}/chaos_calmer/15.05.1"
-for release in $CURRENT_STABLE_RELEASES; do sync_openwrt "http://downloads.openwrt.org/releases/$release/targets" "${TUNASYNC_WORKING_DIR}/release/$release"; done
+sync_openwrt "http://downloads.openwrt.org/chaos_calmer/15.05.1" "${TUNASYNC_WORKING_DIR}/chaos_calmer/15.05.1" 
