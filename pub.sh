44673378b656e1fe8769fe7fadb9fff0d6bd1b23
Hui Yiqun <i@huiyiqun.me>
2019-02-25 13:12:47 +0800

    dart-pub: move PUB_CACHE outside of /root/

--- pub.sh
+++ pub.sh
@@ -4,4 +4,4 @@ set -e
 BASE_URL=${MIRROR_BASE_URL:-"https://mirrors.tuna.tsinghua.edu.cn/dart-pub"}
 UPSTREAM_URL=${TUNASYNC_UPSTREAM_URL:-"https://pub.dartlang.org/api"}
 
-/root/.pub-cache/bin/pub_mirror --upstream $UPSTREAM_URL --verbose --connections 10 --concurrency 100 "$TUNASYNC_WORKING_DIR" "$BASE_URL"
+/pub-cache/bin/pub_mirror --upstream $UPSTREAM_URL --verbose --connections 10 --concurrency 100 "$TUNASYNC_WORKING_DIR" "$BASE_URL"
