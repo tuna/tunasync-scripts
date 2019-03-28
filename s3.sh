67caef3af9724fde87b491b7f1fed18b3b7a5be3
Yuxiang Zhang <z4yx@users.noreply.github.com>
2019-03-28 22:52:45 +0800

    add variable TUNASYNC_AWS_OPTIONS

--- s3.sh
+++ s3.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 [ ! -d "${TUNASYNC_WORKING_DIR}" ] && mkdir -p "${TUNASYNC_WORKING_DIR}"
-aws --delete --no-sign-request --endpoint-url="${TUNASYNC_S3_ENDPOINT}" s3 sync "${TUNASYNC_UPSTREAM_URL}" "${TUNASYNC_WORKING_DIR}"
+aws ${TUNASYNC_AWS_OPTIONS} --delete --no-sign-request --endpoint-url="${TUNASYNC_S3_ENDPOINT}" s3 sync "${TUNASYNC_UPSTREAM_URL}" "${TUNASYNC_WORKING_DIR}"
