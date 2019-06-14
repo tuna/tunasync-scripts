f18602186e3325dffcb5462e457b6e18cf1f0d27
Yuxiang Zhang <z4yx@users.noreply.github.com>
2019-06-14 22:56:21 +0800

    Update anaconda.py
    add more channels mentioned in tuna/issues#112

--- anaconda.py
+++ anaconda.py
@@ -33,6 +33,9 @@ CONDA_CLOUD_REPOS = (
     "menpo/linux-64", "menpo/osx-64", "menpo/win-64", "menpo/win-32", "menpo/noarch",
     "pytorch/linux-64", "pytorch/osx-64", "pytorch/win-64", "pytorch/win-32", "pytorch/noarch", "peterjc123/win-64", "peterjc123/noarch",
     "stackless/linux-64","stackless/win-64","stackless/win-32","stackless/linux-32","stackless/osx-64",
+    "fermi/noarch", "fermi/linux-64", "fermi/osx-64",
+    "fastai/linux-64", "fastai/osx-64", "fastai/win-64", "fastai/noarch",
+    "omnia/linux-64", "omnia/osx-64", "omnia/win-64", "omnia/noarch",
 )
 
 logging.basicConfig(
