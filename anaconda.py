3d5352b132603d8b6408d1f1e846c62addbc073b
Yuxiang Zhang <z4yx@users.noreply.github.com>
2019-07-04 21:27:08 +0800

    add psi4 channel

--- anaconda.py
+++ anaconda.py
@@ -44,6 +44,7 @@ CONDA_CLOUD_REPOS = (
     "intel/linux-64", "intel/linux-32", "intel/osx-64", "intel/win-64", "intel/win-32",
     "auto/linux-64", "auto/linux-32", "auto/osx-64", "auto/win-64", "auto/win-32",
     "ursky/linux-64", "ursky/osx-64",
+    "psi4/linux-64", "psi4/osx-64", "psi4/win-64", "psi4/noarch",
 )
 
 EXCLUDED_PACKAGES = (
