93887406ebc6a89b61ce5975d05e35cb5293abbd
root <root@mirrors>
2020-02-13 21:16:57 +0800

    change the connection option for pub-mirror

--- pub.sh
+++ pub.sh
@@ -3,5 +3,5 @@ set -e
 
 BASE_URL=${MIRROR_BASE_URL:-"https://mirrors.tuna.tsinghua.edu.cn/dart-pub"}
 UPSTREAM_URL=${TUNASYNC_UPSTREAM_URL:-"https://pub.dartlang.org/api"}
-
-/pub-cache/bin/pub_mirror --upstream $UPSTREAM_URL --verbose --connections 1 --concurrency 10 "$TUNASYNC_WORKING_DIR" "$BASE_URL"
+echo "From $UPSTREAM_URL to $BASE_URL"
+exec /pub-cache/bin/pub_mirror --upstream "$UPSTREAM_URL" --verbose --connections 10 --concurrency 10 "$TUNASYNC_WORKING_DIR" "$BASE_URL"
