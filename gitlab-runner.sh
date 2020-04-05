d81ce9a967b7b31b0c0add9d7da4a617a84d3eaa
z4yx <z4yx@users.noreply.github.com>
2020-04-05 22:14:59 +0800

    migrating gitlab-runner,grafana

--- gitlab-runner.sh
+++ gitlab-runner.sh
@@ -5,6 +5,7 @@ set -o pipefail
 
 _here=`dirname $(realpath $0)`
 apt_sync="${_here}/apt-sync.py" 
+yum_sync="${_here}/yum-sync.py"
 
 BASE_PATH="${TUNASYNC_WORKING_DIR}"
 UPSTREAM=${TUNASYNC_UPSTREAM_URL:-"https://packages.gitlab.com/runner/gitlab-runner"}
@@ -13,52 +14,8 @@ YUM_PATH="${BASE_PATH}/yum"
 UBUNTU_PATH="${BASE_PATH}/ubuntu/"
 DEBIAN_PATH="${BASE_PATH}/debian/"
 
-mkdir -p $UBUNTU_PATH $DEBIAN_PATH $YUM_PATH
-
-cache_dir="/tmp/yum-gitlab-runner-cache/"
-cfg="/tmp/gitlab-runner-yum.conf"
-cat <<EOF > ${cfg}
-[main]
-keepcache=0
-
-[el6]
-name=gitlab-runner-el6
-baseurl=https://packages.gitlab.com/runner/gitlab-runner/el/6/x86_64
-repo_gpgcheck=0
-gpgcheck=0
-enabled=1
-gpgkey=https://packages.gitlab.com/gpg.key
-sslverify=0
-
-[el7]
-name=gitlab-runner-el7
-baseurl=https://packages.gitlab.com/runner/gitlab-runner/el/7/x86_64
-repo_gpgcheck=0
-gpgcheck=0
-enabled=1
-gpgkey=https://packages.gitlab.com/gpg.key
-sslverify=0
-
-[el8]
-name=gitlab-runner-el8
-baseurl=https://packages.gitlab.com/runner/gitlab-runner/el/8/x86_64
-repo_gpgcheck=0
-gpgcheck=0
-enabled=1
-gpgkey=https://packages.gitlab.com/gpg.key
-sslverify=0
-EOF
-
-if [[ -z ${DRY_RUN:-} ]]; then
-	reposync -c $cfg -d -p ${YUM_PATH}  -e $cache_dir
-	[ ! -d ${YUM_PATH}/el6 ] && mkdir -p ${YUM_PATH}/el6
-	[ ! -d ${YUM_PATH}/el7 ] && mkdir -p ${YUM_PATH}/el7
-	[ ! -d ${YUM_PATH}/el8 ] && mkdir -p ${YUM_PATH}/el8
-	createrepo --update -v -c $cache_dir -o ${YUM_PATH}/el6 ${YUM_PATH}/el6
-	createrepo --update -v -c $cache_dir -o ${YUM_PATH}/el7 ${YUM_PATH}/el7
-	createrepo --update -v -c $cache_dir -o ${YUM_PATH}/el8 ${YUM_PATH}/el8
-fi
-rm $cfg
+"$yum_sync" "${BASE_URL}/el/@{os_ver}/@{arch}" 6-8 gitlab-runner x86_64 "el@{os_ver}" "$YUM_PATH"
+echo "YUM finished"
 
 "$apt_sync" "${UPSTREAM}/ubuntu" @ubuntu-lts main amd64,i386 "$UBUNTU_PATH"
 echo "Ubuntu finished"
--- grafana.sh
+++ grafana.sh
@@ -4,9 +4,8 @@ set -e
 set -o pipefail
 
 _here=`dirname $(realpath $0)`
-. ${_here}/helpers/apt-download
-
-[ -z "${LOADED_APT_DOWNLOAD}" ] && (echo "failed to load apt-download"; exit 1)
+apt_sync="${_here}/apt-sync.py" 
+yum_sync="${_here}/yum-sync.py"
 
 BASE_PATH="${TUNASYNC_WORKING_DIR}"
 BASE_URL=${TUNASYNC_UPSTREAM_URL:-"https://packages.grafana.com/oss"}
@@ -14,48 +13,11 @@ BASE_URL=${TUNASYNC_UPSTREAM_URL:-"https://packages.grafana.com/oss"}
 YUM_PATH="${BASE_PATH}/yum"
 APT_PATH="${BASE_PATH}/apt"
 
-APT_VERSIONS=("stable" "beta")
-RPM_VERSIONS=("rpm" "rpm-beta")
-
-mkdir -p ${YUM_PATH} ${APT_PATH}
-
-
 # =================== APT repos ===============================
-if [[ ! -z ${DRY_RUN:-} ]]; then
-	export APT_DRY_RUN=1
-fi
-base_url="${BASE_URL}/deb"
-for version in "${APT_VERSIONS[@]}"; do
-	for arch in "amd64" "arm64" "armhf"; do
-		apt-download-binary ${base_url} "$version" "main" "$arch" "${APT_PATH}" || true
-	done
-done
+"$apt_sync" "${BASE_URL}/deb" stable,beta main amd64,armhf,arm64 "$APT_PATH"
 echo "APT finished"
 
 
 # =================== YUM/DNF repos ==========================
-
-cache_dir="/tmp/yum-grafana-cache/"
-cfg="/tmp/yum-grafana.conf"
-cat <<EOF > ${cfg}
-[main]
-keepcache=0
-
-EOF
-
-for rpmver in "${RPM_VERSIONS[@]}"; do
-cat << EOF >> $cfg
-[${rpmver}]
-name=${rpmver}
-baseurl=${BASE_URL}/$rpmver
-enabled=1
-EOF
-done
-
-if [[ -z ${DRY_RUN:-} ]]; then
-	reposync -c $cfg -d -p ${YUM_PATH} -e $cache_dir
-	for rpmver in "${RPM_VERSIONS[@]}"; do
-		createrepo --update -v -c $cache_dir -o ${YUM_PATH}/${rpmver}/ ${YUM_PATH}/${rpmver}/
-	done
-fi
-rm $cfg
+"$yum_sync" "${BASE_URL}/@{comp}" 7 rpm,rpm-beta x86_64 "@{comp}" "$YUM_PATH"
+echo "YUM finished"
