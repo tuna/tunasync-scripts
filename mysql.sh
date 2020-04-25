2da266497b3186d4119cb9627c481e07fdff2b96
z4yx <z4yx@users.noreply.github.com>
2020-04-25 09:36:37 +0800

    configuable MYSQL_RSYNC_UPSTREAM

--- mysql.sh
+++ mysql.sh
@@ -9,9 +9,9 @@ yum_sync="${_here}/yum-sync.py"
 
 BASE_PATH="${TUNASYNC_WORKING_DIR}"
 BASE_URL="${TUNASYNC_UPSTREAM_URL:-"https://repo.mysql.com"}"
+MYSQL_RSYNC_UPSTREAM="${DOWNLOADS_RSYNC_UPSTREAM:-rsync://ftp5.gwdg.de/pub/linux/mysql/Downloads/}"
 
 MYSQL_DOWNLOAD_PATH="${BASE_PATH}/downloads/"
-MYSQL_RSYNC_UPSTREAM="rsync://ftp5.gwdg.de/pub/linux/mysql/Downloads/"
 RSYNC_OPTS="-aHvh --no-o --no-g --stats --exclude .~tmp~/ --delete --delete-excluded --delete-after --delay-updates --safe-links --timeout=120 --contimeout=120"
 USE_IPV6=${USE_IPV6:-"0"}
 if [[ $USE_IPV6 == "1" ]]; then
