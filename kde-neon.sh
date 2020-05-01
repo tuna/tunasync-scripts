7b9b783c8a53d60752d3178ef4110a6d5ccff93a
z4yx <z4yx@users.noreply.github.com>
2020-05-01 09:18:18 +0800

    add KDE Neon sync script

---
+++ kde-neon.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+_here=`dirname $(realpath $0)`
+apt_sync="${_here}/apt-sync.py"
+
+BASE_PATH="${TUNASYNC_WORKING_DIR}"
+BASE_URL=${TUNASYNC_UPSTREAM_URL:-"http://archive.neon.kde.org"}
+
+APT_PATH="${BASE_PATH}/user"
+export REPO_SIZE_FILE=/tmp/reposize.$RANDOM
+
+# =================== APT repos ===============================
+"$apt_sync" --delete "${BASE_URL}/user" @ubuntu-lts main amd64 "${BASE_PATH}/user"
+echo "APT finished"
+
+"${_here}/helpers/size-sum.sh" $REPO_SIZE_FILE --rm
