9d1c2fc8c514b964033998b1a19f4ec12842968b
z4yx <z4yx@users.noreply.github.com>
2020-05-23 15:46:33 +0800

    Update s3.sh

--- s3.sh
+++ s3.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 [[ ! -d "${TUNASYNC_WORKING_DIR}" ]] && mkdir -p "${TUNASYNC_WORKING_DIR}"
+mkdir /tmp/none; cd /tmp/none # enter an empty folder, so the stars in TUNASYNC_AWS_OPTIONS are not expanded
 exec aws --no-sign-request --endpoint-url="${TUNASYNC_S3_ENDPOINT}" s3 sync ${TUNASYNC_AWS_OPTIONS} "${TUNASYNC_UPSTREAM_URL}" "${TUNASYNC_WORKING_DIR}"
