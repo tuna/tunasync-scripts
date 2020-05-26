50a6f0dfbf0f438a53e04ff519724ce7917e8694
z4yx <z4yx@users.noreply.github.com>
2020-05-26 09:33:16 +0800

    override env

--- julia.sh
+++ julia.sh
@@ -3,5 +3,8 @@ set -e
 BASE_URL=${TUNASYNC_UPSTREAM_URL:-"pkg.julialang.org"}
 [[ -d "${TUNASYNC_WORKING_DIR}" ]]
 cd "${TUNASYNC_WORKING_DIR}"
+
+export JULIA_STATIC_DIR="$PWD/static"
+export JULIA_CLONES_DIR="$PWD/clones"
 exec julia -e "using StorageServer; mirror_tarball(\"registries/General\", [\"$BASE_URL\"])"
 
