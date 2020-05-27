cd20ed71b64357fdd4e8d5f7575ef399be15535c
z4yx <z4yx@users.noreply.github.com>
2020-05-27 15:50:51 +0800

    enable --overwrite (tuna/issues/840)

--- pub.sh
+++ pub.sh
@@ -4,4 +4,4 @@ set -e
 BASE_URL=${MIRROR_BASE_URL:-"https://mirrors.tuna.tsinghua.edu.cn/dart-pub"}
 UPSTREAM_URL=${TUNASYNC_UPSTREAM_URL:-"https://pub.dartlang.org/api"}
 echo "From $UPSTREAM_URL to $BASE_URL"
-exec /pub-cache/bin/pub_mirror --upstream "$UPSTREAM_URL" --verbose --delete --connections 10 --concurrency 10 "$TUNASYNC_WORKING_DIR" "$BASE_URL"
+exec /pub-cache/bin/pub_mirror --upstream "$UPSTREAM_URL" --verbose --overwrite --delete --connections 10 --concurrency 10 "$TUNASYNC_WORKING_DIR" "$BASE_URL"
