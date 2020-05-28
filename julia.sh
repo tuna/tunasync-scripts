b14eac015bc3fe813d8d37cdf7eff54a5b15e526
Johnny Chen <johnnychen94@hotmail.com>
2020-05-28 09:17:30 +0800

    use StorageServer#v0.1.0.beta and remove jill dependency

--- julia.sh
+++ julia.sh
@@ -6,5 +6,9 @@ cd "${TUNASYNC_WORKING_DIR}"
 
 export JULIA_STATIC_DIR="$PWD/static"
 export JULIA_CLONES_DIR="$PWD/clones"
+
+# update and mirror the General registry
+git -C registries/General fetch --all
+git -C registries/General reset --hard origin/master
 exec julia -e "using StorageServer; mirror_tarball(\"registries/General\", [\"$BASE_URL\"])"
 
