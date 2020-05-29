306960e39b0b4643fbbdc290791c57eb20a9df7d
Johnny Chen <johnnychen94@hotmail.com>
2020-05-29 12:58:24 +0800

    skip packages that unexpectedly take a long time

--- julia.sh
+++ julia.sh
@@ -7,8 +7,12 @@ cd "${TUNASYNC_WORKING_DIR}"
 export JULIA_STATIC_DIR="$PWD/static"
 export JULIA_CLONES_DIR="$PWD/clones"
 
+# timeout (seconds) for individual package instead of the whole mirror process
+# initialization should use a larger timeout, e.g., 7200
+PKG_TIMEOUT=600
+
 # update and mirror the General registry
 git -C registries/General fetch --all
 git -C registries/General reset --hard origin/master
-exec julia -e "using StorageServer; mirror_tarball(\"registries/General\", [\"$BASE_URL\"])"
+exec julia -e "using StorageServer; mirror_tarball(\"registries/General\", [\"$BASE_URL\"]; timeout=$PKG_TIMEOUT)"
 
