048962c1e0c548703e692c32353f83c8a6192fee
z4yx <z4yx@users.noreply.github.com>
2020-06-02 19:48:31 +0800

    refactor

--- llvm-apt.sh
+++ llvm-apt.sh
@@ -11,9 +11,8 @@ BASE_URL=${TUNASYNC_UPSTREAM_URL:-"https://apt.llvm.org"}
 export REPO_SIZE_FILE=/tmp/reposize.$RANDOM
 
 for os in "xenial" "bionic" "focal" "stretch" "buster"; do
-    for ver in "" "-9" "-10"; do
-        "$apt_sync" --delete "$BASE_URL/$os" llvm-toolchain-$os$ver main amd64 "$BASE_PATH/$os"
-    done
+    prefix=llvm-toolchain-$os
+    "$apt_sync" --delete "$BASE_URL/$os" $prefix,$prefix-9,$prefix-10 main amd64 "$BASE_PATH/$os"
 done
 
 echo "APT finished"
