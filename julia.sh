b5318e694711e197f2344135ef4ac818f83286af
Johnny Chen <johnnychen94@hotmail.com>
2020-08-09 22:38:25 +0800

    [Julia] print relevant information when start julia jobs

--- julia.sh
+++ julia.sh
@@ -11,6 +11,9 @@ REGISTRY_UUID="23338594-aafe-5451-b93e-139f81909106"
 REGISTRY_UPSTREAM="https://github.com/JuliaRegistries/General"
 REGISTRY="(\"$REGISTRY_NAME\", \"$REGISTRY_UUID\", \"$REGISTRY_UPSTREAM\")"
 
+julia -e "using InteractiveUtils; versioninfo(); @show DEPOT_PATH LOAD_PATH"
+julia -e "using Pkg; Pkg.status(\"StorageMirrorServer\")"
+
 # For more usage of `mirror_tarball`, please refer to
 # https://github.com/johnnychen94/StorageMirrorServer.jl/blob/master/examples/gen_static_full.example.jl
 exec julia -e "using StorageMirrorServer; mirror_tarball($REGISTRY, [\"$BASE_URL\"], \"$OUTPUT_DIR\")"
