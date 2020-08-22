0c997d883f06d4aed3d4ce527056cb85453bc22a
Harry Chen <i@harrychen.xyz>
2020-08-22 19:43:53 +0800

    Do not specify s3 endpoint when TUNASYNC_S3_ENDPOINT is not set
    Signed-off-by: Harry Chen <i@harrychen.xyz>

--- s3.sh
+++ s3.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [[ ! -z "${TUNASYNC_S3_ENDPOINT}" ]]; then
+	ENDPOINT="--endpoint-url=${TUNASYNC_S3_ENDPOINT}"
+else
+	ENDPOINT=""
+fi
+
 [[ ! -d "${TUNASYNC_WORKING_DIR}" ]] && mkdir -p "${TUNASYNC_WORKING_DIR}"
 mkdir /tmp/none; cd /tmp/none # enter an empty folder, so the stars in TUNASYNC_AWS_OPTIONS are not expanded
-exec aws --no-sign-request --endpoint-url="${TUNASYNC_S3_ENDPOINT}" s3 sync ${TUNASYNC_AWS_OPTIONS} "${TUNASYNC_UPSTREAM_URL}" "${TUNASYNC_WORKING_DIR}"
+exec aws --no-sign-request ${ENDPOINT} s3 sync ${TUNASYNC_AWS_OPTIONS} "${TUNASYNC_UPSTREAM_URL}" "${TUNASYNC_WORKING_DIR}"
+
