3a33d4e152e3565c0a4dd0b89f0966316a9f2df0
z4yx <z4yx@users.noreply.github.com>
2021-03-21 10:05:29 +0800

    update the upstream of OpenMediaVault

--- openmediavault.sh
+++ openmediavault.sh
@@ -9,18 +9,16 @@ apt_sync="${_here}/apt-sync.py"
 BASE_PATH="${TUNASYNC_WORKING_DIR}"
 BASE_URL=${TUNASYNC_UPSTREAM_URL:-"https://packages.openmediavault.org/public"}
 DISTS=arrakis-proposed,arrakis,usul-proposed,usul
-EXTRA_PREFIX=(arrakis-beta arrakis-testing arrakis usul-testing usul-extras usul-beta usul)
+EXTRA_DISTS=arrakis-docker,arrakis-plex,arrakis-sync,arrakis-teamviewer,arrakis,erasmus-backports,erasmus-beta,erasmus-ce-docker,erasmus-hwraid,erasmus-plex,erasmus-sync,erasmus-teamviewer,erasmus,usul-beta,usul-extras,usul-testing,usul
 ARCHS=amd64,i386,arm64,armel,armhf
 export REPO_SIZE_FILE=/tmp/reposize.$RANDOM
 
 # =================== official repos ===============================
 "$apt_sync" --delete "${BASE_URL}" "$DISTS" main,partner $ARCHS "${BASE_PATH}/public"
+"$apt_sync" --delete "https://openmediavault.github.io/packages" "$DISTS" main,partner $ARCHS "${BASE_PATH}/packages"
 # =================== extra repos ===============================
-for i in "${EXTRA_PREFIX[@]}"
-do
-    "$apt_sync" --delete "https://dl.bintray.com/openmediavault-plugin-developers/$i" \
-        @debian-latest2 main $ARCHS "${BASE_PATH}/openmediavault-plugin-developers/$i"
-done
+"$apt_sync" --delete "https://openmediavault-plugin-developers.github.io/packages/debian/$i" \
+    "$EXTRA_DISTS" main $ARCHS "${BASE_PATH}/openmediavault-plugin-developers"
 echo "APT finished"
 
 "${_here}/helpers/size-sum.sh" $REPO_SIZE_FILE --rm
