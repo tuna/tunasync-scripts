ed075641e9cbab4d018df4f9dc83f50d1327bcee
z4yx <z4yx@users.noreply.github.com>
2021-03-21 10:21:59 +0800

    fix scripts

--- openmediavault.sh
+++ openmediavault.sh
@@ -9,7 +9,7 @@ apt_sync="${_here}/apt-sync.py"
 BASE_PATH="${TUNASYNC_WORKING_DIR}"
 BASE_URL=${TUNASYNC_UPSTREAM_URL:-"https://packages.openmediavault.org/public"}
 DISTS=arrakis-proposed,arrakis,usul-proposed,usul
-EXTRA_DISTS=arrakis-docker,arrakis-plex,arrakis-sync,arrakis-teamviewer,arrakis,erasmus-backports,erasmus-beta,erasmus-ce-docker,erasmus-hwraid,erasmus-plex,erasmus-sync,erasmus-teamviewer,erasmus,usul-beta,usul-extras,usul-testing,usul
+EXTRA_DISTS=arrakis-docker,arrakis-plex,arrakis-sync,arrakis-teamviewer,arrakis,usul-beta,usul-extras,usul-testing,usul
 ARCHS=amd64,i386,arm64,armel,armhf
 export REPO_SIZE_FILE=/tmp/reposize.$RANDOM
 
@@ -17,7 +17,8 @@ export REPO_SIZE_FILE=/tmp/reposize.$RANDOM
 "$apt_sync" --delete "${BASE_URL}" "$DISTS" main,partner $ARCHS "${BASE_PATH}/public"
 "$apt_sync" --delete "https://openmediavault.github.io/packages" "$DISTS" main,partner $ARCHS "${BASE_PATH}/packages"
 # =================== extra repos ===============================
-"$apt_sync" --delete "https://openmediavault-plugin-developers.github.io/packages/debian/$i" \
+wget -O "${BASE_PATH}/openmediavault-plugin-developers/omvextras2026.asc" https://openmediavault-plugin-developers.github.io/packages/debian/omvextras2026.asc
+"$apt_sync" --delete "https://openmediavault-plugin-developers.github.io/packages/debian" \
     "$EXTRA_DISTS" main $ARCHS "${BASE_PATH}/openmediavault-plugin-developers"
 echo "APT finished"
 
