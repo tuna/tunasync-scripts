5f80770a4de263b38dcc836c4c5e37003b0fe5c4
Miao Wang <shankerwangmiao@gmail.com>
2021-06-18 15:55:04 +0800

    termux: change upstream to packages.termux.org

--- termux.sh
+++ termux.sh
@@ -9,13 +9,20 @@ export REPO_SIZE_FILE=/tmp/reposize.$RANDOM
 
 ARCH_LIST="aarch64,arm,i686,x86_64"
 
-"$apt_sync" --delete "https://cf-ipfs.com/ipns/k51qzi5uqu5dg9vawh923wejqffxiu9bhqlze5f508msk0h7ylpac27fdgaskx" stable main    $ARCH_LIST "${WORKING_DIR}/termux-packages-24"
-"$apt_sync" --delete "https://cf-ipfs.com/ipns/k51qzi5uqu5dj05z8mr958kwvrg7a0wqouj5nnoo5uqu1btnsljvpznfaav9nk" unstable main  $ARCH_LIST "${WORKING_DIR}/unstable-packages"
-"$apt_sync" --delete "https://cf-ipfs.com/ipns/k51qzi5uqu5dgu3homski160l4t4bmp52vb6dbgxb5bda90rewnwg64wnkwxj4" x11 main       $ARCH_LIST "${WORKING_DIR}/x11-packages"
-"$apt_sync" --delete "https://cf-ipfs.com/ipns/k51qzi5uqu5dhvbtvdf46kkhobzgamhiirte6s6k28l2c1iapumphh3cpkw33f" science stable $ARCH_LIST "${WORKING_DIR}/science-packages-24"
-"$apt_sync" --delete "https://cf-ipfs.com/ipns/k51qzi5uqu5dhngjg68o8x9uimwy5h8iqt91n2266idc7uet9ew3lc472upy27" games stable   $ARCH_LIST "${WORKING_DIR}/game-packages-24"
-"$apt_sync" --delete "https://cf-ipfs.com/ipns/k51qzi5uqu5dlp5yjlahzcp3kfpnhbifo9ka9iybo3bp5vt781duafkyyvt9al" root stable    $ARCH_LIST "${WORKING_DIR}/termux-root-packages-24"
+"$apt_sync" --delete "https://packages.termux.org/apt/termux-main"     stable main    $ARCH_LIST "${WORKING_DIR}/termux-packages-24"
+"$apt_sync" --delete "https://packages.termux.org/apt/termux-unstable" unstable main  $ARCH_LIST "${WORKING_DIR}/unstable-packages"
+"$apt_sync" --delete "https://packages.termux.org/apt/termux-x11"      x11 main       $ARCH_LIST "${WORKING_DIR}/x11-packages"
+"$apt_sync" --delete "https://packages.termux.org/apt/termux-science"  science stable $ARCH_LIST "${WORKING_DIR}/science-packages-24"
+"$apt_sync" --delete "https://packages.termux.org/apt/termux-games"    games stable   $ARCH_LIST "${WORKING_DIR}/game-packages-24"
+"$apt_sync" --delete "https://packages.termux.org/apt/termux-root"     root stable    $ARCH_LIST "${WORKING_DIR}/termux-root-packages-24"
 
+mkdir -p "${WORKING_DIR}/apt"
+ln -fsTr "${WORKING_DIR}/termux-packages-24"         "${WORKING_DIR}/apt/termux-main"
+ln -fsTr "${WORKING_DIR}/unstable-packages"          "${WORKING_DIR}/apt/termux-unstable"
+ln -fsTr "${WORKING_DIR}/x11-packages"               "${WORKING_DIR}/apt/termux-x11"
+ln -fsTr "${WORKING_DIR}/science-packages-24"        "${WORKING_DIR}/apt/termux-science"
+ln -fsTr "${WORKING_DIR}/game-packages-24"           "${WORKING_DIR}/apt/termux-games"
+ln -fsTr "${WORKING_DIR}/termux-root-packages-24"    "${WORKING_DIR}/apt/termux-root"
 echo "finished"
 
 "${_here}/helpers/size-sum.sh" $REPO_SIZE_FILE --rm
