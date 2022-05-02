0e381dda2aac21d54869fd268281ff1f34d4f37d
Zenithal <i@zenithal.me>
2022-05-03 00:42:01 +0800

    termux: remove deprecated repo
    Ref to https://github.com/tuna/issues/issues/1480

--- termux.sh
+++ termux.sh
@@ -10,18 +10,12 @@ export REPO_SIZE_FILE=/tmp/reposize.$RANDOM
 ARCH_LIST="aarch64,arm,i686,x86_64"
 
 "$apt_sync" --delete "https://packages.termux.org/apt/termux-main"     stable main    $ARCH_LIST "${WORKING_DIR}/termux-packages-24"
-"$apt_sync" --delete "https://packages.termux.org/apt/termux-unstable" unstable main  $ARCH_LIST "${WORKING_DIR}/unstable-packages"
 "$apt_sync" --delete "https://packages.termux.org/apt/termux-x11"      x11 main       $ARCH_LIST "${WORKING_DIR}/x11-packages"
-"$apt_sync" --delete "https://packages.termux.org/apt/termux-science"  science stable $ARCH_LIST "${WORKING_DIR}/science-packages-24"
-"$apt_sync" --delete "https://packages.termux.org/apt/termux-games"    games stable   $ARCH_LIST "${WORKING_DIR}/game-packages-24"
 "$apt_sync" --delete "https://packages.termux.org/apt/termux-root"     root stable    $ARCH_LIST "${WORKING_DIR}/termux-root-packages-24"
 
 mkdir -p "${WORKING_DIR}/apt"
 ln -fsTr "${WORKING_DIR}/termux-packages-24"         "${WORKING_DIR}/apt/termux-main"
-ln -fsTr "${WORKING_DIR}/unstable-packages"          "${WORKING_DIR}/apt/termux-unstable"
 ln -fsTr "${WORKING_DIR}/x11-packages"               "${WORKING_DIR}/apt/termux-x11"
-ln -fsTr "${WORKING_DIR}/science-packages-24"        "${WORKING_DIR}/apt/termux-science"
-ln -fsTr "${WORKING_DIR}/game-packages-24"           "${WORKING_DIR}/apt/termux-games"
 ln -fsTr "${WORKING_DIR}/termux-root-packages-24"    "${WORKING_DIR}/apt/termux-root"
 echo "finished"
 
