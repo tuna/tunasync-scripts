ee09a58cabc58067fbe7ddb28780e7e53f6fe1e4
ZenithalHourlyRate <i@zenithal.me>
2022-07-02 16:31:11 +0800

    elpa: fix mepla stable being melpa
    Related to https://github.com/sjtug/mirror-requests/issues/282

--- elpa.sh
+++ elpa.sh
@@ -15,7 +15,7 @@ UPSTREAMS=(
   "rsync://elpa.gnu.org/elpa/"
   "rsync://elpa.nongnu.org/nongnu/"
   "rsync://melpa.org/packages/"
-  "rsync://stable.melpa.org/packages/"
+  "rsync://stable.melpa.org/packages-stable/"
   )
 
 REPOS=(
