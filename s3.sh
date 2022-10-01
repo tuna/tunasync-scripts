5e2ab0566ac26651a31922ffbaeb9ec924a1ed15
ZenithalHourlyRate <i@zenithal.me>
2022-10-01 23:59:35 +0800

    s3: use --exact-timestamps by default
    Closes https://github.com/tuna/issues/issues/1593
    Ref to https://github.com/aws/aws-cli/issues/3415

--- s3.sh
+++ s3.sh
@@ -8,5 +8,5 @@ fi
 
 [[ ! -d "${TUNASYNC_WORKING_DIR}" ]] && mkdir -p "${TUNASYNC_WORKING_DIR}"
 mkdir /tmp/none; cd /tmp/none # enter an empty folder, so the stars in TUNASYNC_AWS_OPTIONS are not expanded
-exec aws --no-sign-request ${ENDPOINT} s3 sync ${TUNASYNC_AWS_OPTIONS} "${TUNASYNC_UPSTREAM_URL}" "${TUNASYNC_WORKING_DIR}"
+exec aws --no-sign-request ${ENDPOINT} s3 sync --exact-timestamps ${TUNASYNC_AWS_OPTIONS} "${TUNASYNC_UPSTREAM_URL}" "${TUNASYNC_WORKING_DIR}"
 
