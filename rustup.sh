deb2c85056f43a21973ba66064419910b7e0fc63
Zenithal <i@zenithal.me>
2022-12-20 11:47:05 +0800

    rustup: make gc time an env var

--- rustup.sh
+++ rustup.sh
@@ -5,7 +5,8 @@ cd "$TUNASYNC_WORKING_DIR"
 echo "rustup sync started"
 
 BASE_URL=${MIRROR_BASE_URL:-"https://mirrors.tuna.tsinghua.edu.cn/rustup"}
+GC=${RUSTUP_GC:-"30"}
 
-/usr/local/cargo/bin/rustup-mirror -u "${BASE_URL}" -m "${TUNASYNC_WORKING_DIR}" --gc 30
+/usr/local/cargo/bin/rustup-mirror -u "${BASE_URL}" -m "${TUNASYNC_WORKING_DIR}" --gc "${GC}"
 echo "finished"
 
