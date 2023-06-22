78b54cdd3164a18d066592b81e82a621190754ba
weearc <qby19981121@gmail.com>
2023-06-22 22:52:58 +0800

    restore deepin-cd.sh deepin.sh fedora.sh rockylinux.sh

---
+++ deepin-cd.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# This script should be a cronjob and should be run a few times a day. (example for /etc/crontab: "0  *  *  *  * root /usr/bin/manjaroreposync").
+# However you can also move this script to "/etc/cron.hourly".
+# To be an official Manjaro Linux mirror and to get access to our rsync server, you have to tell us your static ip of your synchronization server.
+
+DESTPATH="/srv/www/deepin-cd/"
+RSYNC=/usr/bin/rsync
+LOCKFILE=/tmp/rsync-deepin-cd.lock
+UPSTREAM_URL="rsync://rsync.deepin.com/releases/"
+
+
+synchronize() {
+	/usr/bin/rsync -rtlivH --delete-after --delay-updates --safe-links --max-delete=1000 --contimeout=60 -vvv "$UPSTREAM_URL"  "$DESTPATH"
+}
+
+
+
+if [ ! -e "$LOCKFILE" ]
+then
+    echo $$ >"$LOCKFILE"
+    synchronize
+else
+    PID=$(cat "$LOCKFILE")
+    if kill -0 "$PID" >&/dev/null
+    then
+        echo "Rsync - Synchronization still running"
+        exit 15
+    else
+        echo $$ >"$LOCKFILE"
+        echo "Warning: previous synchronization appears not to have finished correctly"
+        synchronize
+	exit 0
+    fi
+fi
+
+rm -f "$LOCKFILE"
---
+++ fedora.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# This script should be a cronjob and should be run a few times a day. (example for /etc/crontab: "0  *  *  *  * root /usr/bin/manjaroreposync").
+# However you can also move this script to "/etc/cron.hourly".
+# To be an official Manjaro Linux mirror and to get access to our rsync server, you have to tell us your static ip of your synchronization server.
+
+DESTPATH="/srv/www/fedora/"
+RSYNC=/usr/bin/rsync
+LOCKFILE=/tmp/rsync-fedora.lock
+UPSTREAM_URL="rsync://ftp.riken.jp/fedora/"
+
+
+synchronize() {
+	/usr/bin/rsync -rtlivH -vvv --delete-after --delay-updates --safe-links --max-delete=1000 --contimeout=60 \
+	--exclude 'releases/*/*/source/' \
+	--exclude 'core/' \
+	--exclude 'development/' \
+	--exclude 'release/test/' \
+	--exclude 'releases/*/Everything/*/debug/' \
+	--exclude 'epel/' \
+	--exclude 'extras/' "$UPSTREAM_URL"  "$DESTPATH"
+}
+
+
+
+if [ ! -e "$LOCKFILE" ]
+then
+    echo $$ >"$LOCKFILE"
+    synchronize
+else
+    PID=$(cat "$LOCKFILE")
+    if kill -0 "$PID" >&/dev/null
+    then
+        echo "Rsync - Synchronization still running"
+        exit 15
+    else
+        echo $$ >"$LOCKFILE"
+        echo "Warning: previous synchronization appears not to have finished correctly"
+        synchronize
+	exit 0
+    fi
+fi
+
+rm -f "$LOCKFILE"
