d08487239035162a9960e658305fa9d55827512f
Jiajie Chen <c@jia.je>
2024-09-01 11:24:47 +0800

    llvm-apt: add 19

--- llvm-apt.sh
+++ llvm-apt.sh
@@ -12,7 +12,7 @@ export REPO_SIZE_FILE=/tmp/reposize.$RANDOM
 
 for os in "jammy" "noble" "bullseye" "bookworm"; do
     prefix=llvm-toolchain-$os
-    "$apt_sync" --delete "$BASE_URL/$os" $prefix,$prefix-9,$prefix-10,$prefix-11,$prefix-12,$prefix-13,$prefix-14,$prefix-15,$prefix-16,$prefix-17,$prefix-18 main amd64,arm64 "$BASE_PATH/$os"
+    "$apt_sync" --delete "$BASE_URL/$os" $prefix,$prefix-9,$prefix-10,$prefix-11,$prefix-12,$prefix-13,$prefix-14,$prefix-15,$prefix-16,$prefix-17,$prefix-18,$prefix-19 main amd64,arm64 "$BASE_PATH/$os"
 done
 
 echo "APT finished"
