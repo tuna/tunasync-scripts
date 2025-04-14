51c4aa4500132612c5e39d8f33bbc60f4e32e3a7
Shengqi Chen <harry-chen@outlook.com>
2025-04-14 17:20:24 +0800

    s3.sh: set AWS_EC2_METADATA_DISABLED=true (fix #183)
    Signed-off-by: Shengqi Chen <harry-chen@outlook.com>

--- s3.sh
+++ s3.sh
@@ -6,6 +6,9 @@ else
 	ENDPOINT=""
 fi
 
+# see tuna/tunasync-scripts#183
+export AWS_EC2_METADATA_DISABLED=true
+
 [[ ! -d "${TUNASYNC_WORKING_DIR}" ]] && mkdir -p "${TUNASYNC_WORKING_DIR}"
 mkdir /tmp/none; cd /tmp/none # enter an empty folder, so the stars in TUNASYNC_AWS_OPTIONS are not expanded
 exec aws --no-sign-request ${ENDPOINT} s3 sync --exact-timestamps ${TUNASYNC_AWS_OPTIONS} "${TUNASYNC_UPSTREAM_URL}" "${TUNASYNC_WORKING_DIR}"
