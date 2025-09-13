962acd7959cb911c144c52353bc3730518630d24
Shengqi Chen <harry-chen@outlook.com>
2025-09-13 16:33:51 +0800

    rudder: update debian / rhel versions
    Signed-off-by: Shengqi Chen <harry-chen@outlook.com>

--- rudder.sh
+++ rudder.sh
@@ -8,7 +8,7 @@ yum_sync="${_here}/yum-sync.py"
 
 BASE_PATH="${TUNASYNC_WORKING_DIR}"
 UPSTREAM=${TUNASYNC_UPSTREAM_URL:-"https://repository.rudder.io"}
-RUDDER_VERS=(4.3 5.0 6.0)
+RUDDER_VERS=(latest)
 
 YUM_PATH="${BASE_PATH}/rpm"
 APT_PATH="${BASE_PATH}/apt"
@@ -16,7 +16,7 @@ export REPO_SIZE_FILE=/tmp/reposize.$RANDOM
 
 components=$(printf ",%s" "${RUDDER_VERS[@]}")
 components=${components:1}
-"$yum_sync" "${UPSTREAM}/rpm/@{comp}/RHEL_@{os_ver}/" 7 $components x86_64 "rudder@{comp}-RHEL_@{os_ver}" "$YUM_PATH"
+"$yum_sync" "${UPSTREAM}/rpm/@{comp}/RHEL_@{os_ver}/" 9,10 $components x86_64 "rudder@{comp}-RHEL_@{os_ver}" "$YUM_PATH"
 echo "YUM finished"
 
 for ver in ${RUDDER_VERS[@]}; do
