5e1c81663aa6ac866528c576ee7429cb45a1d5d1
Shengqi Chen <harry-chen@outlook.com>
2025-09-18 21:44:11 +0800

    mysql: change to latest apt repos & yum components
    Signed-off-by: Shengqi Chen <harry-chen@outlook.com>

--- mysql.sh
+++ mysql.sh
@@ -17,15 +17,15 @@ UBUNTU_PATH="${APT_PATH}/ubuntu"
 DEBIAN_PATH="${APT_PATH}/debian"
 
 # =================== APT repos ===============================
-MYSQL_APT_REPOS="mysql-5.7,mysql-tools,connector-python-2.1,mysql-8.0"
+MYSQL_APT_REPOS="mysql-tools,mysql-8.0,mysql-8.4-lts"
 "$apt_sync" --delete "${BASE_URL}/apt/ubuntu" @ubuntu-lts $MYSQL_APT_REPOS amd64,i386 "${UBUNTU_PATH}"
 echo "Ubuntu finished"
 "$apt_sync" --delete "${BASE_URL}/apt/debian" @debian-current $MYSQL_APT_REPOS amd64,i386 "${DEBIAN_PATH}"
 echo "Debian finished"
 
 # =================== YUM/DNF repos ==========================
-COMPONENTS="mysql-connectors-community,mysql-tools-community,mysql-8.0-community,mysql-5.7-community"
-"$yum_sync" "${BASE_URL}/yum/@{comp}/el/@{os_ver}/@{arch}/" 7 "$COMPONENTS" x86_64,aarch64 "@{comp}-el@{os_ver}-@{arch}" "$YUM_PATH"
+COMPONENTS="mysql-connectors-community,mysql-tools-community,mysql-8.0-community,mysql-8.4-community"
+"$yum_sync" "${BASE_URL}/yum/@{comp}/el/@{os_ver}/@{arch}/" @rhel-current "$COMPONENTS" x86_64,aarch64 "@{comp}-el@{os_ver}-@{arch}" "$YUM_PATH"
 echo "YUM finished"
 
 "${_here}/helpers/size-sum.sh" $REPO_SIZE_FILE --rm
