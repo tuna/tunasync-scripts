61df7688d9b82d2a4022b291596689f4ebb18e7c
Shengqi Chen <harry-chen@outlook.com>
2025-10-10 14:35:39 +0800

    adoptium: add Java 25, fix tuna/issues#2328
    Signed-off-by: Shengqi Chen <harry-chen@outlook.com>

--- adoptium.py
+++ adoptium.py
@@ -11,7 +11,7 @@ import requests
 DOWNLOAD_TIMEOUT = int(os.getenv('DOWNLOAD_TIMEOUT', '1800'))
 BASE_PATH = os.getenv('TUNASYNC_WORKING_DIR')
 BASE_URL = os.getenv('TUNASYNC_UPSTREAM_URL', "https://packages.adoptium.net/artifactory")
-FEATURE_VERSIONS = [8, 11, 17, 20, 21]
+FEATURE_VERSIONS = [8, 11, 17, 21, 25]
 
 def download_file(url: str, dst_file: Path)->bool:
     try:
