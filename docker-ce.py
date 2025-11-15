tuna/tunasync-scripts

commit 28e9dd3f819ea12b702afdd40e6cf87b48b863d9

panic if failed to get the file list

docker-ce.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import os
+import sys
 import threading
 import queue
 import traceback
@@ -52,7 +53,11 @@ class RemoteSite:
             yield base_url
             return
 
-        r = requests.get(base_url, timeout=TIMEOUT_OPTION)
+        try:
+            r = requests.get(base_url, timeout=TIMEOUT_OPTION)
+        except:
+            print("Panic: failed to get file list");
+            sys.exit(1)
         if not r.ok:
             return
 

commit e8e5ce3f0622d11901c66e4cc5f3773258b6858c

docker-ce: not quitting when panic [ci skip]
Fixes: 28e9dd3

docker-ce.py
@@ -78,7 +78,7 @@ class RemoteSite:
         except Exception as e:
             print("Panic: failed to get file list")
             traceback.print_exc()
-            sys.exit(1)
+            os._exit(1)
         if not r.ok:
             return
 