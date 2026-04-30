9327b047ff7a828077dbd4ffc517179bceede287
Miao Wang <shankerwangmiao@gmail.com>
2026-04-30 20:36:40 +0800

    apt: add resolute

--- apt-sync.py
+++ apt-sync.py
@@ -44,7 +44,7 @@ if USE_ADDR_FAMILY != "":
     )
 
 OS_TEMPLATE = {
-    "ubuntu-lts": ["jammy", "noble"],
+    "ubuntu-lts": ["jammy", "noble", "resolute"],
     "debian-current": ["bullseye", "bookworm", "trixie"],
     "debian-latest2": ["bookworm", "trixie"],
     "debian-latest": ["trixie"],
